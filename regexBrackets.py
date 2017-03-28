c4a239ef544d7601143659c1279d7fb97aff7996
Lenore Alford <topplethepat@users.noreply.github.com>
2017-03-28 14:31:25 -0700

    Add files via upload
    I created this script to automate an aspect of my workflow: using command line parameters, it reads a file and then takes out some bracketed and other items using Regex, and then writes it to a results file.

---
+++ regexBrackets.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# regexBrackets finds [0:00:00] pattern and removes it in a .txt file
+# it also removes speaker mentions, e.g. SPEAKER: F2
+
+#command line example
+#  python regexBrackets.py --list madLibs.txt --results MLres.txt
+
+import argparse
+import re
+
+def main():
+	parser = argparse.ArgumentParser(
+		description = "script to clean files for testing"
+	)
+	parser.add_argument(
+		'--list', 
+		help = "path to txt file", 
+		required = True
+	)
+
+	parser.add_argument(
+		'--results',
+		help = "path to output txt file of cleaned up text",
+		required = True
+		)
+
+	args = parser.parse_args()
+
+	cleanTxt(args.list, args.results)
+
+def cleanTxt (list_path, results_path):
+
+	with open (list_path,'r') as myText:
+		data=myText.read()
+		with open (results_path, 'w') as results_file:
+			new = re.sub(r'\[.*?\]','', data)
+			improved = re.sub(r'SPEAKER\:\s\w\d\d','', new)
+		
+			results_file.write(improved)
+				
+	
+
+
+if __name__ == "__main__":
+	main()
+
+
