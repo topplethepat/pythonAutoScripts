736fab0ea20ef1fdb83ebe9b146128dab6c74166
Lenore Alford <topplethepat@users.noreply.github.com>
2017-07-14 16:42:21 -0700

    Update sampleregexSWBD.py

--- sampleregexSWBD.py
+++ sampleregexSWBD.py
@@ -13,8 +13,6 @@ for filename in os.listdir(path):
 	if filename.endswith(".txt"):
 		with open (filename, 'r') as myText:
 			data = myText.read()
-			#formula for other folder starting with en
-			#done = re.sub(r'\d{1,8}-\d{1,4}_\d{1,7}\s\d\s\w{1,7}\s{1,3}\d{1,3}\.\d{1,3}\s\d{1,3}\.\d{1,3}\s\<\w\,\w{1,2}\,\w{1,4}\>','', data)
 			#formula for swbd
 			done = re.sub(r'\w{1,7}\s\d\s\w\s\d{1,3}\.\d{1,3}\s\d{1,3}\.\d{1,3}\s\<\w\,\w{1,2}\,\w{1,4}\>','', data)
 
