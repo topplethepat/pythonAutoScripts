d8b54e50c57a9d9bdfb4a154372e7137c75b7ed4
Lenore Alford <topplethepat@users.noreply.github.com>
2017-07-14 15:17:41 -0700

    Add files via upload
    This file looks for a time-stamp pattern and then adds newlines before and after it, which saves time reformatting files if we have a particular bug in our system.

---
+++ newline_regex.py
@@ -0,0 +1,33 @@
+#program that adds two new lines whenever there is a
+# [00:00:16] and then saves the new fixed file without timestamps.
+#LA June 2017
+
+
+import re
+import os
+
+path = "/Users/lenorealford/Documents/pythonScripts/test_new_line"
+for filename in os.listdir(path):
+	if filename.endswith(".txt"):
+		with open (filename, 'r') as myText:
+			data = myText.read()
+
+			
+			new = re.sub(r'\[.*?\]', r'\n\g<0>\n\n', data)
+			with open (filename, 'w') as myText:
+				myText.write(new)
+				
+
+	
+    			
+        		
+            	
+
+		
+	
+	
+
+
+				
+
+		 	
