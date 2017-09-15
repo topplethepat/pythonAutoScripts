6f8eed3d9e530b7fd2eb193b372100480d67b962
Lenore Alford <topplethepat@users.noreply.github.com>
2017-09-15 11:08:01 -0700

    Add files via upload
    This script iterates over audio files in a directory and computes their length in seconds. Using pandas.DataFrame, it outputs the results with two headers. Thanks to 
    https://stackoverflow.com/questions/7833807/get-wav-file-length-or-duration for the formula for converting into seconds.

---
+++ newfindLength.py
@@ -0,0 +1,32 @@
+import os
+
+import pandas
+
+path="/Users/lenorealford/Documents/pythonScripts/testVoicemailDownload"
+res_file = "test4VM.csv"
+counter = 0
+data = pandas.DataFrame([])
+for filename in os.listdir(path):
+	if filename.endswith(".wav"):
+		with open (filename, 'r') as f:
+			f.seek(28)
+			a = f.read(4)
+			byteRate=0
+				
+			for i in range(4):
+				byteRate=byteRate + ord(a[i])*pow(256,i)
+					
+				fileSize=os.path.getsize(filename)
+					
+				ms=((fileSize-44)*1000)/byteRate
+				sec = ms/1000
+				counter += 1
+			data = data.append(pandas.DataFrame({'Filename':filename, 'Sec': sec},index = [counter]), ignore_index=True)
+			
+				
+			with open(res_file, 'w') as results:
+				results.write(str(data))
+				
+				
+				
+
