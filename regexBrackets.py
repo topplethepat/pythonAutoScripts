d32a71f50bd188a112dabe75f09a6c93b9878c62
Lenore Alford <topplethepat@users.noreply.github.com>
2020-03-10 13:21:57 -0700

    added comments to explain regex

--- regexBrackets.py
+++ regexBrackets.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # regexBrackets finds [0:00:00] pattern and removes it in a .txt file
-# it also removes speaker mentions, e.g. SPEAKER: F2
+# it also removes speaker mentions like 'SPEAKER: F2'
 
 #command line example
 #  python regexBrackets.py --list madLibs.txt --results ./res.txt
@@ -29,21 +29,15 @@ def main():
 	cleanTxt(args.list, args.results)
 
 def cleanTxt (list_path, results_path):
-
 	with open (list_path,'r') as myText:
 		data = myText.read()
 		with open (results_path, 'w') as results_file:
-			new = re.sub(r'\[.*?\]','', data)
+			new = re.sub(r'\[.*?\]','', data) #removes anything between brackets
 			improved = re.sub(r'SPEAKER\:\s\w{1,3}','', new)
-			#new = re.sub(r'SPEAKER\:\s\w\d', '', data)
-		
 			results_file.write(improved)
 			#results_file.write(new)
 			results_file.close()
 				
-	
-
-
 if __name__ == "__main__":
 	main()
 
